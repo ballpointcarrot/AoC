8dd583d6a6c60152cdeeb3ec6b263b721649857a
Christopher Kruse <ckruse@ballpointcarrot.net>
2019-11-29 08:47:46 -0800

    Initial Commit
    Created a base clj project.

---
+++ test/ballpointcarrot/aoc_test.clj
@@ -0,0 +1,7 @@
+(ns ballpointcarrot.aoc-test
+  (:require [clojure.test :refer :all]
+            [ballpointcarrot.aoc :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
