5b699b9a44e164569b3cb0b444693262babc245a
Christopher Kruse <ckruse@ballpointcarrot.net>
2019-12-01 05:08:37 +0000

    Added Clojure details from Repl.it

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject proj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+                [org.clojure/clojure "1.8.0"]
+                [org.clojure/tools.nrepl "0.2.12"]
+                [clojure-complete "0.2.4"]
+                ]
+  :plugins [[lein-exec "0.3.6"]])
